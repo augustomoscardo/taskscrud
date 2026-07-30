f395e6eb1afe3e09b77c46b706fbc7451fe009ef
Augusto Moscardo <augustomsaluz@gmail.com>
2026-07-30 20:06:44 -0300

    chore: create task model

---
+++ src/model/task.js
@@ -0,0 +1,10 @@
+export class Task {
+  constructor(id, title, description, completedAt, createdAt, updatedAt) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.completedAt = completedAt;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
